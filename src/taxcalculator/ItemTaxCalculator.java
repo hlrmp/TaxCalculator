648bdf910b22c2fed8646acb8fe76629c4f5607a
hlrmp-danmdg <134066063+hlrmp@users.noreply.github.com>
2024-01-05 15:03:06 +0800

    Add files via upload

---
+++ src/taxcalculator/ItemTaxCalculator.java
@@ -0,0 +1,10 @@
+package taxcalculator;
+
+	public class ItemTaxCalculator {
+		
+	public double CalculateItemTax (double itemPrice, double taxPercent) {
+	double decrease = taxPercent / 10.0;
+	return itemPrice * decrease;
+	}
+	
+}
